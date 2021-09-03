c080873c6bee2004f7b15999ff59916aeabf9e83
Emre Başkaya <56039156+emreb3457@users.noreply.github.com>
2021-09-03 20:09:28 +0300

    x

--- config/database.js
+++ config/database.js
@@ -1,7 +1,18 @@
 const mongoose = require("mongoose")
 
-module.exports = () => {
-    mongoose.connect(process.env.DB_LOCAL_URI).then(con => {
-        console.log(`MongoDB Database connected with HOST: ${con.connection.host}`)
-    })
+if (process.env.NODE_ENV === 'DEVELOPMENT') {
+
+    module.exports = () => {
+        mongoose.connect(process.env.DB_LOCAL_URI).then(con => {
+            console.log(`MongoDB Database connected with HOST: ${con.connection.host}`)
+        })
+    }
 }
+else{
+    module.exports = () => {
+        mongoose.connect(process.env.DB_URI).then(con => {
+            console.log(`MongoDB Database connected with HOST: ${con.connection.host}`)
+        })
+    }
+}
+
